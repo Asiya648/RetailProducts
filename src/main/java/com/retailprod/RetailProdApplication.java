fcd6cebd5b188246face7052aa62fbba1bd2a028
Asiya648 <54121631+Asiya648@users.noreply.github.com>
2019-08-15 12:32:31 +0100

    commit

---
+++ src/main/java/com/retailprod/RetailProdApplication.java
@@ -0,0 +1,14 @@
+package com.retailprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailProdApplication.class, args);
+	}
+
+}
+
