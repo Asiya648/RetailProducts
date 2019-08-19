c70cb7b715cef6a890af2ae6ebc6e332de3616da
Asiya648 <54121631+Asiya648@users.noreply.github.com>
2019-08-19 02:42:55 +0100

    commit

--- src/main/java/com/retailprod/services/DiscountService.java
+++ src/main/java/com/retailprod/services/DiscountService.java
@@ -15,6 +15,6 @@ public interface DiscountService {
 	 * @return List<ProductModel>
 	 */
 	
-	List<ProductModel> getDiscountedProducts(Integer categoryId, LabelTypeEnum priceLabelType);
+	List<ProductModel> getDiscountedProducts(Integer categoryId, Optional<LabelTypeEnum> priceLabelType);
 
 }
